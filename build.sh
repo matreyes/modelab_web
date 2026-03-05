0569213673520a5c8379befb5f7660a8529bf279
Matias Reyes <matreyes@gmail.com>
2026-03-05 18:13:41 +0000

    chore: add build.sh for Cloudflare Pages deployment

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Download zola if not available
+if ! command -v zola &> /dev/null; then
+    ZOLA_VERSION="0.22.1"
+    echo "Downloading zola $ZOLA_VERSION..."
+    curl -sL "https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz" \
+        | tar -xz -C /usr/local/bin
+fi
+
+echo "Zola version: $(zola --version)"
+
+# Build CSS
+npm install
+npm run css:build
+
+# Build site
+zola build
